608d4b15aa3437d4bcb5aa72f205ff1db90e3013
Jorge Gonçalves <jorge.a.goncalves@capgemini.com>
2022-04-13 23:12:19 -0300

    Add Challenge Second with constraints

--- 3-challenge-1-and-2-timer-interval-without-let-and-var.js
+++ 3-challenge-1-and-2-timer-interval-without-let-and-var.js
@@ -5,14 +5,31 @@ const recursiveSetTimeout = function(callback, time_pulse, time_in_miliseconds =
     }
 }
 
-//Challenge 1: Print "Hello World" without let or var, incrementing 1 second each time
-recursiveSetTimeout(function(current_time) {
-    console.log(`Hello World: ${current_time/1000}`);
-    return true;
-}, 1000);
+function challengesOneAndTwo() {
 
-//Challenge 2 with Challenge 1 constraints: Print "Hello World " + delay, repeating for 5 times, for each 100 miliseconds increment in delay
-recursiveSetTimeout(function messageRepeat5Times(current_time) {
-    [...Array(5)].forEach((() => console.log(`Hello World. ${current_time}`)));
-    return current_time < 300;
-}, 100);
+    //Challenge 1: Print "Hello World" without let or var, incrementing 1 second each time
+    recursiveSetTimeout(function(current_time) {
+        console.log(`Hello World: ${current_time/1000}`);
+        return true;
+    }, 1000);
+
+    //Challenge 2 with Challenge 1 constraints: Print "Hello World " + delay, repeating for 5 times, for each 100 miliseconds increment in delay
+    recursiveSetTimeout(function messageRepeat5Times(current_time) {
+        [...Array(5)].forEach((() => console.log(`Hello World. ${current_time}`)));
+        return current_time < 300;
+    }, 100);
+
+}
+
+
+//Challenge 2:
+const recursiveSetInterval = function recursiveSetInterval(time = 0, time_pulse = 100, ids_setInterval_list = []) {
+    time += time_pulse
+    ids_setInterval_list.push(setInterval(recursiveSetInterval, time, time, time_pulse, ids_setInterval_list));
+    [...Array(5)].forEach((() => console.log(`Hello World. ${time}`)));
+    if (time >= 300) {
+        ids_setInterval_list.forEach(value => clearInterval(value))
+    }
+}
+
+recursiveSetInterval();
